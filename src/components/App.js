7a8274541b4d0bfc0115e09d616ad4a1aef071b2
Kushal Ghosh <kushalghosh075@gmail.com>
2024-05-12 22:29:53 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Tooltip text={"This is a tooltip"}>
+          <div className="text">Hover over me</div>
+        </Tooltip>
+        <Tooltip text={"This is another tooltip"}>
+          <div className="text">Hover over me to see another tooltip</div>
+        </Tooltip>
     </div>
   )
 }
---
+++ src/components/Tooltip.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './../styles/App.css';
+
+const Tooltip = ({text, children }) => {
+    const [isHide, setIsHide] = useState(true);
+  return (
+    <div className='container'>
+        {!isHide && <div className="tooltiptext">{text}</div>}
+        <div className="tooltip" onMouseEnter={()=>setIsHide(false)} onMouseLeave={()=>setIsHide(true)}>{children}</div>
+    </div>
+  )
+}
+
+export default Tooltip
